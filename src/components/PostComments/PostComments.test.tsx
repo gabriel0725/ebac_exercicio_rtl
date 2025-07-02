a31a23b6887b890bded95c2b6fee0f3ad8e7b1df
Clark <galaxy.games199@gmail.com>
2025-07-02 20:45:15 -0300

    criado teste para inserção de comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,25 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    })
+
+    test('Deve adicionar 2 comentarios', () => {
+        render(<PostComment />);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: { value: 'Muito bonito!' }
+        });
+        
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: { value: 'Parabéns pelo post!' }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        expect(screen.getByText('Muito bonito!')).toBeInTheDocument();
+        expect(screen.getByText('Parabéns pelo post!')).toBeInTheDocument();
+    })
 });
